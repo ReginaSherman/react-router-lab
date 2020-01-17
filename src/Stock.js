df2f029a8a4540021bf98018500b78ec6aa5df0a
kchia <kchia87@gmail.com>
2020-01-17 17:58:12 -0500

    create part II solution

--- src/Stock.js
+++ src/Stock.js
@@ -12,27 +12,6 @@ class Stock extends Component {
       });
   }
   render() {
-    // let stock;
-    // for (let i = 0; i < data.length; i++) {
-    //   if (data[i].symbol === this.props.selectedStock) {
-    //     stock = data[i];
-    //   }
-    // }
-
-    // let stock = data.find(stock => stock.symbol === this.props.selectedStock);
-    // return (
-    //   <div>
-    //     <h2>
-    //       {stock.name} {stock.symbol}
-    //     </h2>
-    //     <ul>
-    //       <li>Current Price: {stock.lastPrice}</li>
-    //       <li>Change: {stock.change}</li>
-    //       <li>High: {stock.high}</li>
-    //       <li>Low: {stock.low}</li>
-    //     </ul>
-    //   </div>
-    // );
     const stock = this.props.selectedStockFromAPI;
     return (
       <div>
