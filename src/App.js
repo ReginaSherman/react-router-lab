ab2455ea8f2301ce0658f9f0b864148892df5327
kchia <kchia87@gmail.com>
2020-04-22 17:38:35 -0400

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import About from "./components/About";
+import Dashboard from "./components/Dashboard";
+import Stock from "./components/Stock";
+import { Link, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-        <div>
-        </div>
+      <div>
+        <nav>
+          <Link to="/stocks">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <Route path="/about" component={About} />
+        <Route path="/stocks" component={Dashboard} />
+        <Route path="/stocks/:symbol" component={Stock} />
+      </div>
     );
   }
 }
---
+++ src/components/Stock.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Stock extends Component {
+  render() {
+    return <h2>This is the Stock page.</h2>;
+  }
+}
+
+export default Stock;
